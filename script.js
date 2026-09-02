0b23a90a03e17c6ef84599338eb66e3a31baccbd
ezequieltech <ezequiel.tech@escola.pr.gov.br>
2026-09-02 20:17:20 -0300

    video 7

--- script.js
+++ script.js
@@ -89,8 +89,13 @@ let perguntaAtual;
 let historiaFinal= "";
 
 function mostraPergunta() {
+  if(atual >= perguntas.length) {
+    mostraResultado();
+    return; 
+  }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
 function mostraAlternativas(){
@@ -105,9 +110,14 @@ function mostraAlternativas(){
 }
 function respostaSelecionada(opcaoSelecionada){
   const afirmacoes = opcaoSelecionada.afirmacoes;
-  historiaFinal = afirmacoes;
+  historiaFinal += afirmacoes + " ";
    atual++;
       mostraPergunta();
     
+}
+function mostraResultado(){
+  caixaPerguntas.textContent = "Em 2049...";
+  textoResultado.textContent = historiaFinal;
+  caixaAlternativas.textContent = "";
 }
 mostraPergunta();
