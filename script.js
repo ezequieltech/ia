446c233f6a8dcf4a646c07110899a42b4291f83d
ezequieltech <ezequiel.tech@escola.pr.gov.br>
2026-09-02 18:33:56 -0300

    video 2

---
+++ script.js
@@ -0,0 +1,24 @@
+const caixaPrincipal = document.querySeletctor(".caixa-principal");
+const caixaPerguntas = document.querySeletctor(".caixa-perguntas");
+const caixaAlternativas = document.querySeletctor(".caixa-alternativas");
+const caixaResultado = document.querySeletctor(".caixa-resultado");
+const textoResultado = document.querySeletctor(".texto-resultado");
+
+const perguntas = [
+    {
+   enunciado: "Pergunta 1",
+alternativas: [
+    "Alternativa 1", 
+    "Alternativa 2"
+]
+},
+ {
+   enunciado: "Pergunta 1",
+alternativas: [
+    "Alternativa 1", 
+    "Alternativa 2"
+]
+}
+];
+
+
